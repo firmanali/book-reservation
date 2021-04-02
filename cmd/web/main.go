32449e0d1ab8c7849720962afe425dc81283d8ac
Firman Ali <firman.ali@iconpln.co.id>
2021-04-02 16:58:09 +0700

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/firmanali/book-reservation/pkg/config"
+	"github.com/firmanali/book-reservation/pkg/handlers"
+	"github.com/firmanali/book-reservation/pkg/render"
+)
+
+const portNumber = ":8888"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache", err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println("Starting on port", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
